eba4aa611586c9ae3097b8e4ce9d9d37db652588
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-05-28 00:26:16 -0700

    setup mongoose connection

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoURI = process.env.MONGO_URI;
 
 const routes = require('./app/routes');
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
 
 // set public folder
 app.use(express.static(path.join(__dirname, 'app/public')));
